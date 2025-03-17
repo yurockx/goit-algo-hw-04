8659d9d5d65d1dad2698787fd2a2b46fe38f543b
yurockx <71849449+yurockx@users.noreply.github.com>
2025-03-17 16:59:50 +0200

    Second_task_done
    Also made some variable adjustments in the first task

--- First_task/salary_calc.py
+++ First_task/salary_calc.py
@@ -2,20 +2,20 @@ import pathlib
 import os
 
 # Function to calculate total and average amount of salary from the file
-def total_salary(filepath):
-    path = pathlib.Path(filepath)
+def total_salary(path):
+    file_path = pathlib.Path(path)
     #checking the_file integrity_and_availability
-    if not path.exists():
-        return f"Error: File '{path}' does not exist."
-    if not path.is_file():
-        return f"Error: '{path}' is not a file."
-    if not os.access(path, os.R_OK):
-        return f"Error: File '{path}' is not readable."
-    if path.stat().st_size == 0:
-        return f"Error: File '{path}' File is empty."
+    if not file_path.exists():
+        return f"Error: File '{file_path}' does not exist."
+    if not file_path.is_file():
+        return f"Error: '{file_path}' is not a file."
+    if not os.access(file_path, os.R_OK):
+        return f"Error: File '{file_path}' is not readable."
+    if file_path.stat().st_size == 0:
+        return f"Error: File '{file_path}' File is empty."
 
     try:
-        with open(path, "r", encoding="utf-8") as file:
+        with open(file_path, "r", encoding="utf-8") as file:
             lines = file.read().splitlines()
             salaries = []
             for line in lines:
@@ -24,7 +24,7 @@ def total_salary(filepath):
                     if part.isdigit():
                         salaries.append(int(part))
             if not salaries:
-                return f"Error: the file {path} doesn't contain salary numbers."
+                return f"Error: the file {file_path} doesn't contain salary numbers."
 
             total_salary = sum(salaries)
             average_salary = total_salary // len(salaries)  # Integer division
@@ -33,7 +33,7 @@ def total_salary(filepath):
     except UnicodeDecodeError:
         return "Unicode error - the file is not valid"
     except ValueError:
-        print(f"Error: Invalid file path format: '{path}'")
+        print(f"Error: Invalid file path format: '{file_path}'")
     except Exception as e:
         return f"Unknown error occurred: {e}."
 
---
+++ Second_task/get_cats_info.py
@@ -0,0 +1,38 @@
+import pathlib
+import os
+
+def get_cats_info(path):
+    file_path = pathlib.Path(path)
+    # checking the_file integrity_and_availability
+    if not file_path.exists():
+        return f"Error: File '{file_path}' does not exist."
+    if not file_path.is_file():
+        return f"Error: '{file_path}' is not a file."
+    if not os.access(file_path, os.R_OK):
+        return f"Error: File '{file_path}' is not readable."
+    if file_path.stat().st_size == 0:
+        return f"Error: File '{file_path}' File is empty."
+
+    try:
+        with open(file_path, "r", encoding="utf-8") as file:
+            lines = file.read().splitlines()
+            list_cats_dics = []
+            dict = {}
+            for line in lines:
+                parts = line.split(',')
+                id = parts[0]
+                name = parts[1]
+                age = parts[2]
+                dict.update({"id": id, "name": name, "age": age})
+                list_cats_dics.append(dict)
+            return list_cats_dics
+
+    except UnicodeDecodeError:
+        return "Unicode error - the file is not valid"
+    except ValueError:
+        print(f"Error: Invalid file path format: '{file_path}'")
+    except Exception as e:
+        return f"Unknown error occurred: {e}."
+
+cats_info = get_cats_info("/Users/yurii/PycharmProjects/goit-algo-hw-04/Second_task/cats.txt")
+print(cats_info)
